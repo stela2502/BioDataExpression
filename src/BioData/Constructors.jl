2e9bd4abede39db3e9fa6261c5828ea115672327
Stefan Lang <Stefan.Lang@med.lu.se>
2018-07-05 09:10:30 +0200

    fixed object name show method

--- src/BioData/Constructors.jl
+++ src/BioData/Constructors.jl
@@ -19,15 +19,17 @@ function BioData( file::String )
       end
       
       db = SQLite.DB(file)
-      so = SQLite.query(db, "SELECT * from datavalues")
+      so = SQLite.query(db, "SELECT  gene_id, cell_id, value from datavalues")
+      #println(so)
       if typeof(so[2]) == DataType(Array{Union{Int64, Missings.Missing},1}) 
         A = sparse( so[1], so[2], so[3] );
       else
         A = sparse( map( Int64,collect(so[1]) ),  map(Int64,collect(so[2])) , map( Int64, collect(so[3]))  )
       end
       samples = SQLite.query(db, "SELECT * from cells")
+      #println(samples)
       annotation = SQLite.query(db, "SELECT * from genes")
-      
+      #println(annotation)
       return ( SingleCellExpr( A, samples, annotation, "Unnamed SingleCellData", 0 , 0, pwd() ) )
 end
 
--- src/BioData/Show.jl
+++ src/BioData/Show.jl
@@ -1,9 +1,11 @@
+
+
 function Base.show(io::IO, m::SingleCellExpr ) 
-  println(string( "And object of class " , typeof( data) , " with " , size( data.data)[2], " Cells and ", size(data.data)[1] , " Genes" )  )
-  if ( typeof(data.raw) != Int64 )
+  println(string( "And object of class " , typeof( m) , " with " , size( m.m)[2], " Cells and ", size(m.m)[1] , " Genes" )  )
+  if ( typeof(m.raw) != Int64 )
     println(string( "The data has been normalized" ))
   end
-  if ( typeof(data.zscored) != Int64 )
+  if ( typeof(m.zscored) != Int64 )
     println(string( "The data has been z-scored" ))
   end
 end
