63789a07eccd651de55a70310e55b014ac426c81
Stefan Lang <Stefan.Lang@med.lu.se>
2018-07-05 09:12:29 +0200

    still buggy!

--- src/BioData/Constructors.jl
+++ src/BioData/Constructors.jl
@@ -20,16 +20,16 @@ function BioData( file::String )
       
       db = SQLite.DB(file)
       so = SQLite.query(db, "SELECT  gene_id, cell_id, value from datavalues")
-      #println(so)
+      println(so)
       if typeof(so[2]) == DataType(Array{Union{Int64, Missings.Missing},1}) 
         A = sparse( so[1], so[2], so[3] );
       else
         A = sparse( map( Int64,collect(so[1]) ),  map(Int64,collect(so[2])) , map( Int64, collect(so[3]))  )
       end
       samples = SQLite.query(db, "SELECT * from cells")
-      #println(samples)
+      println(samples)
       annotation = SQLite.query(db, "SELECT * from genes")
-      #println(annotation)
+      println(annotation)
       return ( SingleCellExpr( A, samples, annotation, "Unnamed SingleCellData", 0 , 0, pwd() ) )
 end
 
